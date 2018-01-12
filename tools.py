d2e5ecc00c760c03bace3348aabd9591dd53657a
Gustavo Isaza <30878977+gisazae@users.noreply.github.com>
2018-01-12 08:56:54 -0500

    Update tools.py

--- tools.py
+++ tools.py
@@ -7,7 +7,7 @@ import os
 """ Convierte ensamblaje ADN a aminoacidos, se utiliza via TransDecoder (https://transdecoder.github.io/), que permita convertir
 un archivo fasta de ADN (ensamblaje o reads) a aminoacidos y que de forma automatica seleccione el marco de lectura mas apropiado 
 (transeq (http://emboss.sourceforge.net/apps/cvs/emboss/apps/transeq.html) convierte a los 6 marcos de lectura 
-pero no permite escoger automaticamente el marco de lectura mas apropiado) """	
+pero no permite escoger automáticamente el marco de lectura más apropiado) """	
 def nucl2amino(nThreads,target):
     if(os.path.exists("transdecoder")):
         os.system("rm -fr transdecoder")
